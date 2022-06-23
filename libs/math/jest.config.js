bad5b1ebcc9b0649c973e9d2a0927d44b05c95f2
Nikita Balikhin <ramiro.cruz@mail.ru>
2022-06-24 00:49:42 +0300

    Feature/library builder (#5)
    * Heft library rig added and used in react-hooks
    
    * [fp] migration to heft-library-rig
    
    * [data-structures] Migration to heft-library-config
    
    * [math] Migration to heft-library-rig
    
    * Changelog

--- libs/math/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-	displayName: {
-		color: 'yellow',
-		name: 'impl',
-	},
-	preset: 'ts-jest/presets/js-with-ts',
-	testEnvironment: 'node',
-};
